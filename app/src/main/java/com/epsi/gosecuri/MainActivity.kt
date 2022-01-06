720061d0c37c762e036703bcb36405fe99d61653
yannis42800 <yannisthomas97>
2022-01-06 18:00:19 +0100

    install de Jsoup pour grapping

--- app/src/main/java/com/epsi/gosecuri/MainActivity.kt
+++ app/src/main/java/com/epsi/gosecuri/MainActivity.kt
@@ -2,10 +2,16 @@ package com.epsi.gosecuri
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.doAsync
+import org.jsoup.Jsoup
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        doAsync {
+            var doc = Jsoup.connect("").get()
+        }
+
     }
 }
